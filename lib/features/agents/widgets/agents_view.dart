fe25ffee548d28c7bc3e1c957a14cb720a12f765
liguanda01 <liguanda01@zuoyebang.com>
2026-07-05 15:29:41 +0800

    chore:移除无用测试文件

--- lib/features/agents/widgets/agents_view.dart
+++ lib/features/agents/widgets/agents_view.dart
@@ -12269,8 +12269,6 @@ class _AgentNumberStepperFieldState extends State<_AgentNumberStepperField> {
                             enabledBorder: InputBorder.none,
                             focusedBorder: InputBorder.none,
                             disabledBorder: InputBorder.none,
-                            filled: false,
-                            fillColor: Colors.transparent,
                             isCollapsed: true,
                             contentPadding: EdgeInsets.symmetric(
                               horizontal: 10,
