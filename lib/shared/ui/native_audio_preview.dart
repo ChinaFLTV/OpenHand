84328335fde0dd8cde4fc32296e18d10b40848a3
liguanda01 <liguanda01@zuoyebang.com>
2026-08-05 18:42:30 +0800

    修正音频释放删除竞态

--- lib/shared/ui/native_audio_preview.dart
+++ lib/shared/ui/native_audio_preview.dart
@@ -1577,7 +1577,7 @@ class _NativeAudioPreviewState extends State<NativeAudioPreview> {
 
   Future<void> _disposePlayerAndTemp() async {
     try {
-      await _player.dispose().timeout(kNativeAudioControlTimeout);
+      await _player.dispose();
     } catch (error, stack) {
       silentLog('native_audio_preview', '释放播放器失败', error, stack);
     } finally {
